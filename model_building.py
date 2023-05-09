4fdff7a75ce79f7bc7bfdcfbad53decfd35f09f8
Guilherme Duarte <g@lcap.co>
2023-05-09 19:12:01 -0300

    Imports at the top of the model building script.

--- model_building.py
+++ model_building.py
@@ -1,6 +1,14 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
-import numpy as np 
+import numpy as np
+from sklearn.model_selection import train_test_split
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error
+import pickle
 
 df = pd.read_csv('eda_data.csv')
 
@@ -14,7 +22,6 @@ df_model = df[['avg_salary','Rating','Size','Type of ownership','Industry','Sect
 df_dum = pd.get_dummies(df_model)
 
 # train test split 
-from sklearn.model_selection import train_test_split
 
 X = df_dum.drop('avg_salary', axis =1)
 y = df_dum.avg_salary.values
@@ -22,14 +29,10 @@ y = df_dum.avg_salary.values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # multiple linear regression 
-import statsmodels.api as sm
 
 X_sm = X = sm.add_constant(X)
 model = sm.OLS(y,X_sm)
 model.fit().summary()
-
-from sklearn.linear_model import LinearRegression, Lasso
-from sklearn.model_selection import cross_val_score
 
 lm = LinearRegression()
 lm.fit(X_train, y_train)
@@ -56,13 +59,11 @@ df_err = pd.DataFrame(err, columns = ['alpha','error'])
 df_err[df_err.error == max(df_err.error)]
 
 # random forest 
-from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor()
 
 np.mean(cross_val_score(rf,X_train,y_train,scoring = 'neg_mean_absolute_error', cv= 3))
 
 # tune models GridsearchCV 
-from sklearn.model_selection import GridSearchCV
 parameters = {'n_estimators':range(10,300,10), 'criterion':('mse','mae'), 'max_features':('auto','sqrt','log2')}
 
 gs = GridSearchCV(rf,parameters,scoring='neg_mean_absolute_error',cv=3)
@@ -76,14 +77,12 @@ tpred_lm = lm.predict(X_test)
 tpred_lml = lm_l.predict(X_test)
 tpred_rf = gs.best_estimator_.predict(X_test)
 
-from sklearn.metrics import mean_absolute_error
 mean_absolute_error(y_test,tpred_lm)
 mean_absolute_error(y_test,tpred_lml)
 mean_absolute_error(y_test,tpred_rf)
 
 mean_absolute_error(y_test,(tpred_lm+tpred_rf)/2)
 
-import pickle
 pickl = {'model': gs.best_estimator_}
 pickle.dump( pickl, open( 'model_file' + ".p", "wb" ) )
 
